9198d801063d4c26db9185e8f75294a40095cce3
Sota Nakamori <delphsot@gmail.com>
2015-04-27 01:26:43 +0900

    fix bug and node information

--- dikstra.pl
+++ dikstra.pl
@@ -6,33 +6,55 @@ use warnings;
 my @nodes = (
     {
         label      => 0,
-        edges_to   => [qw/1 2 3/],
-        edges_cost => [0, 5, 4, 2, 0, 0],
+        edges_to   => [qw/1 5/],
     },
     {
         label      => 1,
-        edges_to   => [qw/0 2 5/],
-        edges_cost => [5, 0, 2, 0, 0, 6],
+        edges_to   => [qw/0 2 6/],
     },
     {
         label      => 2,
-        edges_to   => [qw/0 1 3 4/],
-        edges_cost => [4, 2, 0, 3, 2, 0],
+        edges_to   => [qw/1 3/],
     },
     {
         label      => 3,
-        edges_to   => [qw/0 2 4/],
-        edges_cost => [2, 0, 3, 0, 6, 0],
+        edges_to   => [qw/2 8/],
     },
     {
         label      => 4,
-        edges_to   => [qw/2 3 5/],
-        edges_cost => [0, 0, 2, 6, 0, 4],
+        edges_to   => [qw/5 9/],
     },
     {
         label      => 5,
-        edges_to   => [qw/1 4/],
-        edges_cost => [0, 6, 0, 0, 4, 0],
+        edges_to   => [qw/0 4 6/],
+    },
+    {
+        label      => 6,
+        edges_to   => [qw/1 5 7/],
+    },
+    {
+        label      => 7,
+        edges_to   => [qw/2 6 8/],
+    },
+    {
+        label      => 8,
+        edges_to   => [qw/3 7 12/],
+    },
+    {
+        label      => 9,
+        edges_to   => [qw/6 10 11/],
+    },
+    {
+        label      => 10,
+        edges_to   => [qw/9 11/],
+    },
+    {
+        label      => 11,
+        edges_to   => [qw/10 12/],
+    },
+    {
+        label      => 12,
+        edges_to   => [qw/8 11/],
     },
 );
 
@@ -59,11 +81,12 @@ sub pop_min {
     return $found;
 }
 
-my $start = 0;
-my $goal  = 5;
-my $large = 1000;
+my $start     = 1;
+my $goal      = 12;
+my $large     = 1000;
+my $edge_cost = 1;
 
-my %leng = map { $_->{label} => ( $_->{label} == 0 ? 0 : $large ) } @nodes;
+my %leng = map { $_->{label} => ( $_->{label} == $start ? 0 : $large ) } @nodes;
 my %prev;
 
 my %target = map { $_->{label} => $_ } @nodes;
@@ -74,10 +97,9 @@ while ( keys %target ) {
 
     foreach my $to_label (@{$node->{edges_to}}) {
         my $to_node = $nodes[$to_label];
-        my $to_cost = $node->{edges_cost}->[$to_label];
 
-        if ( $leng{$to_label} > $leng{$label} + $to_cost ) {
-            $leng{$to_label} = $leng{$label} + $to_cost;
+        if ( $leng{$to_label} > $leng{$label} + $edge_cost ) {
+            $leng{$to_label} = $leng{$label} + $edge_cost;
             $prev{$to_label} = $label;
         }
     }
